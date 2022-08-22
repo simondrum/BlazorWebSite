595d83b62f7e859da49f3b4b345d60973121f0cb
simon <registerandothers@tutanota.com>
2022-08-22 14:05:18 +0200

    initial commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            
+        }
+        public void ShowDialog()
+        {
+            var win = new WpfApp1.MainWindow();
+            win.ShowDialog();
+        }
+    }
+}
